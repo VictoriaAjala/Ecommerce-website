91ce43230cff842c1b6e5ae263ab0ab4a199f983
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-01-16 12:37:03 +0100

    navbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+//  search section
  search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () =>{
     search.classList.toggle('active');
     cart.classList.remove('active');
     user.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let cart = document.querySelector('.cart');
@@ -12,6 +14,7 @@ document.querySelector('#cart-icon').onclick = () =>{
     cart.classList.toggle('active');
     search.classList.remove('active');
     user.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let user = document.querySelector('.user');
@@ -20,7 +23,31 @@ document.querySelector('#user-icon').onclick = () =>{
     user.classList.toggle('active');
     search.classList.remove('active');
     cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+
+window.onscroll = () => {
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+//  Navbar screen
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0)
+});
 
 var swiper = new Swiper(".new-arrival", {
     spaceBetween: 30,
